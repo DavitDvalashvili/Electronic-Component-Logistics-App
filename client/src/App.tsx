f13986317a2fc24dab06714c52bb402688d89eec
davit dvalashvili <davitdvalashvili1996@gmail.com>
2024-09-09 03:25:38 +0400

    refactor: refactor code in the app.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Components from "./pages/Components";
@@ -7,20 +14,32 @@ import Device from "./pages/Device";
 import Devices from "./pages/Devices";
 import Header from "./components/Layout/Header";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route index path="/components" element={<Components />} />
+      <Route path="/component/:id" element={<Component />} />
+      <Route path="/devices" element={<Devices />} />
+      <Route path="/device/:id" element={<Device />} />
+      <Route path="*" element={<Navigate to="/components" replace />} />
+    </Route>
+  )
+);
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="app font-bpg relative">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route index element={<Components />} />
-          <Route path="/component/:id" element={<Component />} />
-          <Route path="/devices" element={<Devices />} />
-          <Route path="/device/:id" element={<Device />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-        <ToastContainer />
-      </BrowserRouter>
+      <RouterProvider router={router} />
+      <ToastContainer />
     </div>
   );
 }
